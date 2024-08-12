9904916859656d1f2aaef3af7cee025bc20c37fb
borkoloco <santiagobosca@gmail.com>
2024-08-12 17:03:35 -0300

    socket

---
+++ models/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.port = process.env.PORT;
+    this.io = socketio(this.server, {
+      /*configuraciones*/
+    });
+  }
+
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    //inicializo middlewares
+    this.middlewares();
+
+    //inicializo sockets
+    this.configurarSockets();
+
+    //inicializo el servidor
+    this.server.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
