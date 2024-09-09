e3a416f8fa321216661108466f16f2d9b74b57ec
borkoloco <santiagobosca@gmail.com>
2024-09-09 03:39:32 -0300

    net

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
-const cors = require("cors");
+// const cors = require("cors");
 
 class Server {
   constructor() {
@@ -19,7 +19,7 @@ class Server {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
 
     //CORS
-    this.app.use(cors());
+    // this.app.use(cors());
   }
 
   configurarSockets() {
